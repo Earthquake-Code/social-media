5fa375392037fc1b2a2759f51f0ab04c439814c3
gfloriano11 <gflorianodev@gmail.com>
2025-02-25 20:47:10 -0300

    chore: remove useless content

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { createBrowserRouter } from 'react-router-dom'
 import { RouterProvider } from 'react-router'
+import App from './App.jsx'
 import Login from './pages/login.jsx';
+import './index.css'
 
 
 const router = createBrowserRouter([
